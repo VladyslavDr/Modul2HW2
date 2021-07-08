e7fc99026d1d9c531606c30cf7f756aeaae369c4
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-07-08 13:33:42 +0300

    Выбрать товары в корзину
    - сделал корзину и добавил товары

---
+++ Modul2HW2(code)/Modul2HW2(code)/Basket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Basket
+{
+    private const int MaxSize = 10;
+    private Product[] _products = new Product[MaxSize];
+    private int _i;
+
+    private Basket()
+    {
+    }
+
+    public static Basket Instance => new Basket();
+
+    public void Add(Product product)
+    {
+        if (_i < MaxSize)
+        {
+            _products[_i++] = product;
+        }
+        else
+        {
+            Console.WriteLine("Корзина расчитана на 10 продуктов!");
+        }
+    }
+}
---
+++ Modul2HW2(code)/Modul2HW2(code)/Starter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Starter
+{
+    public static void Run()
+    {
+        var products = ProductProvider.GetArrayProducts();
+        var basket = Basket.Instance;
+
+        basket.Add(products[0]);
+        basket.Add(products[3]);
+        basket.Add(products[4]);
+        basket.Add(products[5]);
+        basket.Add(products[8]);
+        basket.Add(products[9]);
+    }
+}
